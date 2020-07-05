88ef5695e2500b40ee9876a4e045b45d371f189a
unknown <ericking2501@gmail.com>
2020-07-05 02:07:09 -0600

    v1.0

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Routes, Route } from 'react-router';
+
+import Ordenes from './components/paginas/Ordenes';
+
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Ordenes />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
