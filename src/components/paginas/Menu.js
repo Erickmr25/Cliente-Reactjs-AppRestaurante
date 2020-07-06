07622c00c3663123369cccb92313f8948da63564
unknown <ericking2501@gmail.com>
2020-07-06 01:03:12 -0600

    v1.3

--- src/components/paginas/Menu.js
+++ src/components/paginas/Menu.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import { Link } from 'react-router-dom';
+import { FirebaseContext } from '../../firebase';
+
+import Platillo from '../ui/Platillo';
 
 const Menu = () => {
+
+    // Definir el state para los platillos
+    const [ platillos, guardarPlatillos ] = useState([]);
+
+    const { firebase } = useContext(FirebaseContext);
+
+
+    // Consultar la base de datos al cargar
+    useEffect(() => {
+        const obtenerPlatillos = () => {
+           firebase.db.collection('productos').onSnapshot(manejarSnapshot);
+        }
+        obtenerPlatillos();
+    }, []);
+
+    // Snapshot nos permite utilizar la base de datos en tiempo real de firestore
+    function manejarSnapshot(snapshot){
+        const platillos = snapshot.docs.map(doc => {
+            return{
+                id: doc.id,
+                ...doc.data()   
+            }
+        });
+        
+        // Guardar los resultados en el state
+        guardarPlatillos(platillos);
+    }
+
     return ( 
         <>
             <h1 className="text-3xl font-light mb-4">Menu</h1>
@@ -9,6 +40,13 @@ const Menu = () => {
             <Link to="/nuevo-platillo" className="bg-blue-800 hover:bg-blue-700, inline-block mb-5 p-2 text-white uppercase font-bold">
                 Agregar Platillo
             </Link>
+
+            {platillos.map( platillo => (
+                <Platillo 
+                    key={platillo.id}
+                    platillo={platillo}
+                />
+                ))}
         </>
      );
 }
